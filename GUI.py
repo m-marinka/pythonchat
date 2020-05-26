12eb986a5610abd20259c35064a9e91cc09b08e1
m-marinka <mmakar569@gmail.com>
2020-05-26 17:09:41 +0300

    add gui

--- GUI.py
+++ GUI.py
@@ -1,5 +1,4 @@
-from tkinter import Frame, Label, Entry, Button, Text
-
+from tkinter import Frame, Label, Entry, Button, Text, messagebox, Tk
 
 class GUI:
     def __init__(self, master):
@@ -14,8 +13,8 @@ class GUI:
 
     def display_name_selection(self):
         frame = Frame()
-        Label(frame, text = 'chat box:', font=("Serif", 12)).pack(side='top', anchor='w')
-        self.name_widget = Entry (frame, width=50, borderwidth=2)
+        Label(frame, text='chat box:', font=("Serif", 12)).pack(side='top', anchor='w')
+        self.name_widget = Entry(frame, width=50, borderwidth=2)
         self.name_widget.pack(side='left', anchor='e')
         self.join_button = Button(frame, text='join', width=10, command=self.on_join).pack(side='left')
         frame.pack(side='top', anchor='w')
@@ -27,3 +26,28 @@ class GUI:
         self.enter_text_widget.pack(side='left', pady=15)
         self.enter_text_widget.bind('<Return>', self.on_enter_key_pressed)
         frame.pack(side='top')
+
+    def on_join(self):
+        if len(self.name_widget.get()) == 0:
+            messagebox.showerror("Enter your name", "Enter your name to send message")
+            return
+        self.name_widget.config(state='disabled')
+
+    def on_enter_key_pressed(self):
+        if len(self.name_widget.get()) == 0:
+            messagebox.showerror("Enter your name", "Enter your name to send message")
+            return
+
+    def on_close_window(self):
+        if messagebox.askokcancel("Quit", "Do you want to exit? "):
+            self.root.destroy()
+            exit(0)
+
+
+# run window here
+
+if __name__ == '__main__':
+    root = Tk()
+    gui = GUI(root)
+    root.protocol('WM_DELETE_WINDOW', gui.on_close_window())
+    root.mainloop()
